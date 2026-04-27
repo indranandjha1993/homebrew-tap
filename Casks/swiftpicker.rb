d7ee0d0712a452208c0f757d11176c6637b07f74
GitHub Action <action@github.com>
2026-04-27 19:00:40 +0000

    Update SwiftPicker to v1.0.0

--- Casks/swiftpicker.rb
+++ Casks/swiftpicker.rb
@@ -2,7 +2,7 @@ cask "swiftpicker" do
   version "1.0.0"
   sha256 :no_check # This should be updated with the actual SHA256 of the zip file
 
-  url "https://github.com/indranandjha1993/SwiftPicker/releases/download/v#{version}/SwiftPicker.app.zip"
+  url "https://github.com/indranandjha1993/SwiftPicker/releases/download/v1.0.0/SwiftPicker.app.zip"
   name "SwiftPicker"
   desc "Modern, native macOS browser picker built with SwiftUI"
   homepage "https://github.com/indranandjha1993/SwiftPicker"

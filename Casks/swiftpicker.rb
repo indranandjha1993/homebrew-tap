e06865f2bc536a2e0fd529199739b4b7df59e069
Indra Nand Jha <indranandjha0@gmail.com>
2026-04-28 00:18:05 +0530

    chore: update username in formula

--- Casks/swiftpicker.rb
+++ Casks/swiftpicker.rb
@@ -2,10 +2,10 @@ cask "swiftpicker" do
   version "1.0.0"
   sha256 :no_check # This should be updated with the actual SHA256 of the zip file
 
-  url "https://github.com/your-username/SwiftPicker/releases/download/v#{version}/SwiftPicker.app.zip"
+  url "https://github.com/indranandjha1993/SwiftPicker/releases/download/v#{version}/SwiftPicker.app.zip"
   name "SwiftPicker"
   desc "Modern, native macOS browser picker built with SwiftUI"
-  homepage "https://github.com/your-username/SwiftPicker"
+  homepage "https://github.com/indranandjha1993/SwiftPicker"
 
   app "SwiftPicker.app"
 
